3dfa3ec5aa5aef5eb880f4f6c845571a3f3f677f
tenznz <dhugkar@gmail.com>
2022-01-21 02:30:17 -0800

    Added: user input requirement

--- employee_payroll.py
+++ employee_payroll.py
@@ -9,12 +9,14 @@ logger.setLevel(logging.DEBUG)
 
 IS_FULL_TIME = 1
 IS_PART_TIME = 2
-EMP_RATE_PER_HOUR = 20
-NUM_OF_WORKING_DAYS = 20
-MAX_HRS_IN_MONTH = 100
 
 
 class EmployeeWage:
+    def __init__(self, max_days, max_hours, rate_per_hours):
+        self.num_of_working_days = max_days
+        self.max_hrs_in_month = max_hours
+        self.emp_rate_per_hour = rate_per_hours
+
     def get_employee_wage(self):
 
         """
@@ -25,7 +27,8 @@ class EmployeeWage:
         emp_wage = 0
         total_hours = 0
         total_wage = 0
-        for i in range(NUM_OF_WORKING_DAYS):
+        logger.info("Checking presence and add wage accordingly")
+        for i in range(self.num_of_working_days):
             is_present = randint(0, 2)
             switch = {
                 1: 8,
@@ -36,10 +39,10 @@ class EmployeeWage:
             total_hours = total_hours + emp_hour
             logger.info(total_hours)
             print("day-", i, "hours-", emp_hour)
-            if total_hours > MAX_HRS_IN_MONTH:
+            if total_hours > self.max_hrs_in_month:
                 break
 
-            emp_wage = emp_hour * EMP_RATE_PER_HOUR
+            emp_wage = emp_hour * self.emp_rate_per_hour
             total_wage = total_wage + emp_wage
         logger.info("Total wage below")
         logger.info(total_wage)
@@ -48,6 +51,15 @@ class EmployeeWage:
 
 
 if __name__ == '__main__':
-    logging.info("UC7")
-    employee1 = EmployeeWage()
-    logger.info(employee1.get_employee_wage())
+    logging.info("UC8")
+    # company_name = input("Enter the company: ")
+    # logger.info(company_name)
+    max_working_days = int(input("Enter number of Working days in a month: "))
+    logger.info("Inserted total working days")
+    max_working_hours = int(input("Enter number of Working hours in a month: "))
+    logger.info("Inserted working hours")
+    emp_rate = int(input("Rate per hours: "))
+    logger.info("Inserted rate per hours")
+    employee1 = EmployeeWage(max_working_days, max_working_hours, emp_rate)
+    print("\nTotal wages:", employee1.get_employee_wage())
+    logger.info("program run successfully..!!")
